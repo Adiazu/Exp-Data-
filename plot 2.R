2cbc79af946fd4fa5a97945ee511d02131a588c1
Adiazu <arnulfodiaz02@gmail.com>
2020-06-14 17:17:32 -0600

    Innitial commit

---
+++ plot 2.R
@@ -0,0 +1,20 @@
+## Loading the libraries I will use
+library(data.table)
+library(sqldf)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+##Total emissions by year in Baltimore
+TotalEmi <- sqldf("Select year,sum(emissions) from NEI
+                    where fips = 24510
+                  group by year")
+##Creating the plot
+plot(TotalEmi$year,TotalEmi$`sum(emissions)`,xlab = "Year", ylab="Total emisions (in tons)", main = "PM2.5 Emissions in Baltimore City" )
+
+
+
+
+##Saving the plot
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
